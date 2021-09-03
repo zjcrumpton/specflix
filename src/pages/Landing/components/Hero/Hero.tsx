bc09a1197f62b8736054ff9f31413f24023a18ed
Zaccary Crumpton <zjcrumpton@gmail.com>
2021-09-03 10:22:14 -0600

    implement hero image

--- src/pages/Landing/components/Hero/Hero.tsx
+++ src/pages/Landing/components/Hero/Hero.tsx
@@ -1,11 +1,20 @@
 import React, { FC } from 'react';
 
+import * as S from './styled';
 import Header from '../Header';
 
 const Hero: FC = () => {
   return (
     <section>
       <Header />
+      <S.BackgroundWrapper>
+        <S.BackgroundGradient />
+
+        <S.BackgroundImg
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/5a27cb25-33a9-4bcc-b441-95fefabcbd37/4d2d33bb-c8d0-4c01-abaa-6aaa031a943e/US-en-20210823-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+          srcSet="https://assets.nflxext.com/ffe/siteui/vlv3/5a27cb25-33a9-4bcc-b441-95fefabcbd37/4d2d33bb-c8d0-4c01-abaa-6aaa031a943e/US-en-20210823-popsignuptwoweeks-perspective_alpha_website_small.jpg 1000w, https://assets.nflxext.com/ffe/siteui/vlv3/5a27cb25-33a9-4bcc-b441-95fefabcbd37/4d2d33bb-c8d0-4c01-abaa-6aaa031a943e/US-en-20210823-popsignuptwoweeks-perspective_alpha_website_medium.jpg 1500w, https://assets.nflxext.com/ffe/siteui/vlv3/5a27cb25-33a9-4bcc-b441-95fefabcbd37/4d2d33bb-c8d0-4c01-abaa-6aaa031a943e/US-en-20210823-popsignuptwoweeks-perspective_alpha_website_large.jpg 1800w"
+        />
+      </S.BackgroundWrapper>
     </section>
   );
 };
--- src/pages/Landing/components/Hero/styled.ts
+++ src/pages/Landing/components/Hero/styled.ts
@@ -1,1 +1,31 @@
 import styled from 'styled-components';
+
+export const BackgroundWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+  overflow: hidden;
+`;
+
+export const BackgroundImg = styled.img`
+  object-fit: cover;
+  transform: none;
+`;
+
+export const BackgroundGradient = styled.div`
+  background: rgba(0, 0, 0, 0.4);
+  background-image: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0.8) 0,
+    rgba(0, 0, 0, 0) 60%,
+    rgba(0, 0, 0, 0.8) 100%
+  );
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
