be7876c4c168492ec2db7e81d27ba69deb4242de
Zaccary Crumpton <zjcrumpton@gmail.com>
2021-09-04 12:12:32 -0600

    implement hero text for landing page

--- src/pages/Landing/components/Hero/components/HeroText/HeroText.tsx
+++ src/pages/Landing/components/Hero/components/HeroText/HeroText.tsx
@@ -3,7 +3,17 @@ import React, { FC } from 'react';
 import * as S from './styled';
 
 const HeroText: FC = () => {
-  return <S.HeroTextContainer />;
+  return (
+    <S.HeroTextContainer>
+      <S.HeroTitle>Unlimited movies, TV shows, and more.</S.HeroTitle>
+      <S.HeroSubtitle>Watch anywhere. Cancel anytime.</S.HeroSubtitle>
+      <S.HeroForm>
+        <S.HeroPrompt>
+          Ready to watch? Enter your email to create or restart your membership.
+        </S.HeroPrompt>
+      </S.HeroForm>
+    </S.HeroTextContainer>
+  );
 };
 
 export default HeroText;
--- src/pages/Landing/components/Hero/components/HeroText/styled.ts
+++ src/pages/Landing/components/Hero/components/HeroText/styled.ts
@@ -11,3 +11,37 @@ export const HeroTextContainer = styled.div`
   font-size: 1.625rem;
   font-weight: 400;
 `;
+
+export const HeroTitle = styled.h1`
+  font-size: 4rem;
+  max-width: 800px;
+  margin: 0 auto;
+  line-height: 1.1;
+  text-align: center;
+  font-weight: 400;
+`;
+
+export const HeroSubtitle = styled.h2`
+  font-size: 1.625rem;
+  margin: 1rem auto;
+  max-width: 800px;
+  font-weight: 400;
+`;
+
+export const HeroForm = styled.form`
+  max-width: 950px;
+  padding-top: 0.85rem;
+  display: flex;
+  flex-direction: column;
+  font-size: 1.625rem;
+  font-weight: 400;
+`;
+
+export const HeroPrompt = styled.h3`
+  max-width: none;
+  padding-bottom: 20px;
+  font-size: 1.2rem;
+  padding: 0 5%;
+  margin: 0 auto;
+  font-weight: 400;
+`;
