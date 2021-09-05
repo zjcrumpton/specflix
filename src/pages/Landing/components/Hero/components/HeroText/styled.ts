503737da645238b80b6fdc3bed4a6478ce3dcc47
Zaccary Crumpton <zjcrumpton@gmail.com>
2021-09-04 18:08:42 -0600

    improve hero responsiveness

--- src/pages/Landing/components/Hero/components/HeroText/styled.ts
+++ src/pages/Landing/components/Hero/components/HeroText/styled.ts
@@ -3,29 +3,35 @@ import styled from 'styled-components';
 export const HeroTextContainer = styled.div`
   position: relative;
   width: 100%;
-  padding: 75px 0;
+  padding: 30px 0;
   max-width: 950px;
   margin: 0 auto;
   text-align: center;
   z-index: 1;
   font-size: 4rem;
   font-weight: 400;
+
+  @media (min-width: 550px) {
+    padding: 75px 0;
+  }
 `;
 
 export const HeroTitle = styled.h1`
   font-size: 1.75rem;
-  max-width: 800px;
+  max-width: 400px;
   margin: 0 auto;
   line-height: 1.1;
   text-align: center;
   font-weight: 400;
 
-  @media (min-width: 1450px) {
-    font-size: 4rem;
+  @media (min-width: 550px) {
+    max-width: 550px;
+    font-size: 3.125rem;
   }
 
-  @media (min-width: 550px) {
-    font-size: 3.125rem;
+  @media (min-width: 1450px) {
+    font-size: 4rem;
+    max-width: 800px;
   }
 `;
 
@@ -50,7 +56,7 @@ export const HeroForm = styled.form`
 `;
 
 export const HeroPrompt = styled.h3`
-  max-width: none;
+  max-width: 300px;
   padding-bottom: 20px;
   font-size: 18px;
   padding: 0 5%;
@@ -60,9 +66,11 @@ export const HeroPrompt = styled.h3`
 
   @media (min-width: 550px) {
     font-size: 23px;
+    max-width: 450px;
   }
 
   @media (min-width: 950px) {
     font-size: 1.2rem;
+    max-width: none;
   }
 `;
